048a3da59846a79b4694b4d0b27984a133f0ab81
zeufack <zeufackp@gmail.com>
2025-07-22 23:13:55 +0200

    improve load time

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,18 @@
 -- Load core settings
 require("core.options")
 require("core.keymaps")
-require("core.barbar_key_map")
 
 local function bootstrap_pckr()
-	local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+    local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
 
-	if not (vim.uv or vim.loop).fs_stat(pckr_path) then
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/lewis6991/pckr.nvim",
-			pckr_path,
-		})
-	end
+    if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+        vim.fn.system({
+            "git", "clone", "--filter=blob:none",
+            "https://github.com/lewis6991/pckr.nvim", pckr_path
+        })
+    end
 
-	vim.opt.rtp:prepend(pckr_path)
+    vim.opt.rtp:prepend(pckr_path)
 end
 
 bootstrap_pckr()
